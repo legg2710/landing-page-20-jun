accfdfb682ffdfa12e5455b87d6c97b1026b5452
legg2710 <luisemilio2710@gmail.com>
2021-06-26 17:13:01 +0000

    90% ready

--- src/js/component/Footer.js
+++ src/js/component/Footer.js
@@ -1,12 +1,11 @@
 import React from "react";
 
 export function Footer() {
-    return (
-
-        <footer className="page-footer font-small bg-dark ">
+	return (
+		<footer className="page-footer font-small bg-dark ">
 			<div className="footer-copyright text-center py-3 text-white">
 				Copyright © LuisG 2021
 			</div>
 		</footer>
-    );
+	);
 }
--- src/js/home.js
+++ src/js/home.js
@@ -1,26 +1,23 @@
 import React from "react";
 import { Navbar } from "./component/Navbar.js";
 import { Jumbotron } from "./component/Jumbotron";
-import { Cards } from "./component/Cards";
+import { Card } from "./component/Card";
 import { Footer } from "./component/Footer";
 //create your first component
 export function Home() {
 	return (
 		<div className="container-fluid m-0 p-0">
-			<React.Fragment>
-				<Navbar />
-				<div className="container">
-					<Jumbotron />
-					<div className="d-flex justify-content-between">
-						<Card />
-						<Card />
-						<Card />
-						<Card />
-					</div>
+			<Navbar />
+			<div className="container">
+				<Jumbotron />
+				<div className="d-flex justify-content-between">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
 				</div>
-				<Footer />
-			</React.Fragment>
-			,
+			</div>
+			<Footer />
 		</div>
 	);
 }
